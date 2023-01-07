ba66ba525463741f1b62ead5791a7c447345689c
tilak7781 <tilakbhati91@gmail.com>
2023-01-08 00:20:06 +0530

    day-8 fraction intrinsic-value min max simple grid

---
+++ Day-8/Intrinsic-values/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("input").forEach((radioInput) =>
+    radioInput.addEventListener("change", function (event) {
+      document
+        .querySelector(".container")
+        .style.setProperty("--col-track-size", event.target.value);
+    })
+  );
+});
